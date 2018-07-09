427ff758e886ef874e030be1ed5f0dca8f4b0ad3
DionysusBenstein <gromovdenis962@gmail.com>
2018-07-09 15:51:54 +0300

    Memory write test

--- MemoryHandler/main.cpp
+++ MemoryHandler/main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
-	char* processName = (char*)"StoneShard.exe";
+	char* processName = (char*)"Telegram.exe";
+	int modifiedNumber = 13;
 
 	MemoryHandler memHandler(processName);
 	memHandler.open();
+	memHandler.write(&modifiedNumber, 0x3A024B8, 4);
 	cout << "Process name: " << processName << "\tProcces ID: " << memHandler.getProcID() << endl;
-	cout << "Read string: " << memHandler.read(0x2FF16104, 4).toNum<int>() << endl;
+	cout << "Read string: " << memHandler.read(0x3A024B8, 4).toNum<int>() << endl;
 	memHandler.close();
 	
 	return 0;
